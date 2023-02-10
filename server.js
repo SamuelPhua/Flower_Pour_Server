89c62ef9c4ad7e7caeb150797f8e8b0589081f0d
mervin-njy <mervin.njy@outlook.com>
2023-02-10 15:58:48 +0800

    app.use(cors())

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./db/db");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
