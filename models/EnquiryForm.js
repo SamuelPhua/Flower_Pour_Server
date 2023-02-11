8426d5eedadfe848d6fe29bc7703e0aa2870120e
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-02-11 14:17:54 +0800

    Added seed.js + Updated Product model to include price
    Updates to Product model:
    1. changed image property to include packaging so that we can display the correct image when different UOM is selected. (stretch goal)
    
    Prepared seed.js - 5 prototypes

--- models/EnquiryForm.js
+++ models/EnquiryForm.js
@@ -7,6 +7,7 @@ const EnquiryFormSchema = new mongoose.Schema(
     email: { type: String },
     phone: { type: String, required: true },
     message: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
--- models/Products.js
+++ models/Products.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     about: {
       ingredients: { type: String },
@@ -10,7 +10,9 @@ const ProductSchema = new mongoose.Schema(
       packaging: { type: String },
       storage: { type: String },
     },
-    image: { type: String }, //Can find out more about the base64
+    price: [{ weight: String, packaging: String, sgdPrice: Number }],
+    image: [{ packaging: String, imageSrc: String }], //Can find out more about the base64
+    // added packaging in the image array - when cust select "Kraft Pouch" -> the corresponding image will show instead
   },
   { timestamps: true }
 );
