9402dd5a0e87b3b8ba89bfcbb2822bb6ac30244d
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-02-11 22:49:57 +0800

    Connection to Atlas established
    Note:
    1. Installed cors
    2. Installed dotenv
    3. Installed mongoose
    4. Moved db connection string to env file (Database name= Cookies)

--- db/db.js
+++ db/db.js
@@ -1,17 +1,21 @@
 /*
-** Username: FlowerPour
-** Password: rGET2A7AXL5QAMqU
-*/
+ ** Username: FlowerPour
+ ** Password: rGET2A7AXL5QAMqU
+ */
 
 const mongoose = require("mongoose");
 
-const db =
-  "mongodb+srv://FlowerPour:rGET2A7AXL5QAMqU@flowerpour.fe5mpxn.mongodb.net/?retryWrites=true&w=majority";
+// MOVED TO PROCESS.ENV
+// const db =
+//   "mongodb+srv://FlowerPour:rGET2A7AXL5QAMqU@flowerpour.fe5mpxn.mongodb.net/?retryWrites=true&w=majority";
 
-const connectDB = async () => {
+const connectDB = async (uri) => {
   try {
-    await mongoose.connect(db);
-    console.log("DB connected");
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Cookies DB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectDB(process.env.MONGODB_URI);
-
 const PORT = process.env.PORT || 5001;
+const URI = process.env.MONGODB_CONNECTION_STRING;
+
+connectDB(URI);
 
 app.listen(PORT, () => {
   console.log(`Server is listeing on ${PORT}`);
