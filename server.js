d81a90f8a5d858d09c704c94ec9efc28443b3e2a
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-02-11 23:56:19 +0800

    Added seedCookies API in server.js
    1. Cookies seeded (Data stored in Cookies(db) -> products (collection name)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/db");
+const Products = require("./models/Products");
+const { products: productSeed } = require("./data/seed");
 const app = express();
 
 app.use(cors());
@@ -13,6 +15,16 @@ const URI = process.env.MONGODB_CONNECTION_STRING;
 
 connectDB(URI);
 
+app.put("/seedCookies", async (req, res) => {
+  try {
+    await Products.deleteMany();
+    await Products.create(productSeed);
+    return res.json({ status: "OK", message: "Seeds planted successfully!" });
+  } catch (err) {
+    return res.status(400).json({ status: "Error", message: err.message });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is listeing on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
