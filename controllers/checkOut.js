c41f04848720e349c3c3b79c81798ccc96137588
mervin-njy <mervin.njy@outlook.com>
2023-02-12 16:29:40 +0800

    checkout completed

--- controllers/checkOut.js
+++ controllers/checkOut.js
@@ -1,49 +1,43 @@
-const Checkout = require("../modals/Checkout");
+const Checkout = require("../models/Checkout");
 
 const createOrder = async (req, res) => {
   try {
-    const newEnquiry = new EnquiryForm({
+    const newOrder = new Checkout({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       phone: req.body.phone,
-      message: req.body.message,
+      deliveryMethod: req.body.deliveryMethod,
+      address: req.body.address,
+      apartment: req.body.apartment,
+      postalCode: req.body.postalCode,
+      deliveryCountry: req.body.deliveryCountry,
+      orderCurrency: req.body.orderCurrency,
+      discountCode: req.body.discountCode,
+      deliveryCharge: req.body.deliveryCharge,
+      checkoutAmount: req.body.checkoutAmount,
+      paymentMethod: req.body.paymentMethod,
+      paymentAmount: req.body.paymentAmount,
+      orderStatus: req.body.orderStatus,
+      cart: {
+        name: req.body.name,
+        weight: req.body.weight,
+        packaging: req.body.packaging,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        itemTotal: req.body.itemTotal,
+      },
     });
 
-    await newEnquiry.save();
-    return res.status(200).json({ status: "OK", message: "Enquiry sent." });
+    await newOrder.save();
+    return res
+      .status(200)
+      .json({ status: "OK", message: "Checkout order sent." });
   } catch (err) {
     return res.status(400).json({ status: "Error", message: err.message });
   }
 };
 
-// {
-//     firstName: { type: String, required: true },
-//     lastName: { type: String, required: true },
-//     email: { type: String, required: true },
-//     phone: { type: String, required: true },
-//     deliveryMethod: { type: String, required: true },
-//     address: { type: String }, // address not required for self-collection
-//     apartment: { type: String },
-//     postalCode: { type: String },
-//     deliveryCountry: { type: String },
-//     orderCurrency: { type: String }, // to default to 'SGD'
-//     discountCode: { type: String },
-//     deliveryCharge: { type: Number },
-//     checkoutAmount: { type: Number, required: true }, // front-end validation: checkoutAmount === paymentAmount (can be 0 if customer use coupons to pay for orders.)
-//     paymentMethod: { type: String },
-//     paymentAmount: { type: Number },
-//     orderStatus: { type: String }, // default to 'O' : 'open' (in case they need to be able to mark order as delivered in future to manage open orders.)
-//     cart: [
-//       {
-//         name: { type: String, required: true },
-//         weight: { type: String },
-//         packaging: { type: String },
-//         price: { type: Number, required: true },
-//         quantity: { type: Number, required: true },
-//         itemTotal: { type: Number, required: true },
-//       },
-//     ],
-//   },
+// const getOrders - for auth users (flourpour admin)
 
 module.exports = { createOrder };
